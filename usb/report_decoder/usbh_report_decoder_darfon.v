ef1f9f89f7316c43c0e86665b3cdda5dc6bfb103
emard <vordah@gmail.com>
2020-03-19 00:56:16 +0100

    darfon report decoder improved

--- usb/report_decoder/usbh_report_decoder_darfon.v
+++ usb/report_decoder/usbh_report_decoder_darfon.v
@@ -27,15 +27,19 @@ module usbh_report_decoder
   wire usbjoy_r      = i_report[ 7:6 ] == 2'b11 ? 1'b1 : 1'b0;
   wire usbjoy_u      = i_report[15:14] == 2'b00 ? 1'b1 : 1'b0;
   wire usbjoy_d      = i_report[15:14] == 2'b11 ? 1'b1 : 1'b0;
-  wire usbjoy_a      = i_report[46] | (i_report[49] & R_autofire[c_autofire_bits-1]);
-  wire usbjoy_b      = i_report[45] | (i_report[51] & R_autofire[c_autofire_bits-1]);
+  wire usbjoy_a      = i_report[46];
+  wire autofire_a    = ((i_report[50] | i_report[49]) & R_autofire[c_autofire_bits-1]); // A | ltrigger | rbumper
+  wire usbjoy_b      = i_report[45];
+  wire autofire_b    = ( i_report[51]                 & R_autofire[c_autofire_bits-1]); // B | rtrigger
   wire usbjoy_start  = i_report[53];
   wire usbjoy_select = i_report[52];
 
+  reg [7:0] R_btn;
   always @(posedge i_clk)
   begin
+    o_btn <= R_btn | {6'b000000, autofire_b, autofire_a};
     if(i_report_valid)
-      o_btn <=
+      R_btn <=
       {
         usbjoy_r, usbjoy_l, usbjoy_d, usbjoy_u,
         usbjoy_start, usbjoy_select, usbjoy_b, usbjoy_a
