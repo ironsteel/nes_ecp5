a00060c01d01e9f1d7a4e2f0f13625c7df24eb02
emard <vordah@gmail.com>
2020-03-25 18:02:58 +0100

    usbh_report_decoder_darfon.v: hat and more keys

--- usb/report_decoder/usbh_report_decoder_darfon.v
+++ usb/report_decoder/usbh_report_decoder_darfon.v
@@ -3,6 +3,9 @@
 // HID report to NES 8-bit button state
 
 // For this core to work properly,
+// set HID to accept strict report of exactly 8 bytes in top.v
+// C_report_length=8, C_report_length_strict=1
+// or maybe workaround:
 // press central silver button toggles upper left LED marked "1"
 // joystick will work reliably when upper left LED marked "1" is OFF
 // if upper left LED marked "1" is ON, fake keypresses will be randomly generated
@@ -24,17 +27,30 @@ module usbh_report_decoder
   always @(posedge i_clk)
     R_autofire <= R_autofire + 1;
 
+  wire [3:0] S_hat = i_report[43:40];
+  reg  [3:0] R_hat_udlr;
+  always @(posedge i_clk)
+    R_hat_udlr <= S_hat == 4'b0000 ? 4'b1000 : // up
+                  S_hat == 4'b0001 ? 4'b1001 : // up+right
+                  S_hat == 4'b0010 ? 4'b0001 : // right
+                  S_hat == 4'b0011 ? 4'b0101 : // down+right
+                  S_hat == 4'b0100 ? 4'b0100 : // down
+                  S_hat == 4'b0101 ? 4'b0110 : // down+left
+                  S_hat == 4'b0110 ? 4'b0010 : // left
+                  S_hat == 4'b0111 ? 4'b1010 : // up+left
+                                     4'b0000 ; // 4'b1111 when not pressed
+
   // darfon/dragonrise joystick report decoder
-  wire usbjoy_l      = i_report[ 7:6 ] == 2'b00 ? 1'b1 : 1'b0;
-  wire usbjoy_r      = i_report[ 7:6 ] == 2'b11 ? 1'b1 : 1'b0;
-  wire usbjoy_u      = i_report[15:14] == 2'b00 ? 1'b1 : 1'b0;
-  wire usbjoy_d      = i_report[15:14] == 2'b11 ? 1'b1 : 1'b0;
-  wire usbjoy_a      = i_report[46];
-  wire autofire_a    = ((i_report[50] | i_report[49]) & R_autofire[c_autofire_bits-1]); // A | ltrigger | rbumper
-  wire usbjoy_b      = i_report[45];
-  wire autofire_b    = ( i_report[51]                 & R_autofire[c_autofire_bits-1]); // B | rtrigger
-  wire usbjoy_start  = i_report[53];
-  wire usbjoy_select = i_report[52]; // button labelled "BACK"
+  wire usbjoy_l      =  (i_report[ 7:6 ] == 2'b00 ? 1'b1 : 1'b0) | R_hat_udlr[1];
+  wire usbjoy_r      =  (i_report[ 7:6 ] == 2'b11 ? 1'b1 : 1'b0) | R_hat_udlr[0];
+  wire usbjoy_u      =  (i_report[15:14] == 2'b00 ? 1'b1 : 1'b0) | R_hat_udlr[3];
+  wire usbjoy_d      =  (i_report[15:14] == 2'b11 ? 1'b1 : 1'b0) | R_hat_udlr[2];
+  wire usbjoy_a      =   i_report[46] | i_report[44]; // A or Y
+  wire autofire_a    = ((i_report[50] | i_report[49]) & R_autofire[c_autofire_bits-1]); // A : ltrigger | rbumper
+  wire usbjoy_b      =   i_report[45] | i_report[47]; // B or X 
+  wire autofire_b    = ((i_report[51] | i_report[48]) & R_autofire[c_autofire_bits-1]); // B : rtrigger | lbumper
+  wire usbjoy_start  =   i_report[53];
+  wire usbjoy_select =   i_report[52]; // button labelled "BACK"
 
   reg [7:0] R_btn;
   always @(posedge i_clk)
