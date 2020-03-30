7277fd839943efba218d3988474414b0dfd044eb
emard <vordah@gmail.com>
2020-03-30 19:58:54 +0200

    flash_loader.v: simplify and support 1MB image like 100in1

--- flash_loader.v
+++ flash_loader.v
@@ -3,26 +3,25 @@
 * from a given offset
 */
 
-module flash_loader(
-  input clock,
-  input reset,
+module flash_loader
+(
+  input  clock,
+  input  reset,
   
-  input reload,
-  input [3:0] index,
+  input  reload,
+  input  [3:0] index,
   
   //Flash load interface
   output flash_csn,
   output flash_sck,
   output flash_mosi,
-  input flash_miso,
+  input  flash_miso,
 
   output [7:0] load_write_data,
   output data_valid
 );
 
 
-// Support games up to 512Kb
-localparam [19:0] END_ADDR = 20'h80000;
 `ifndef SIM
 localparam [23:0] FLASH_BEGIN_ADDR = 24'h200000;
 `else
@@ -31,7 +30,7 @@ localparam [23:0] FLASH_BEGIN_ADDR = 24'h000000;
 
 
 reg load_done;
-reg [19:0] load_addr;
+reg [21:0] load_addr; // Support games up to 1MB (MSB=1 ends)
 
 wire flashmem_valid = !load_done;
 wire flashmem_ready;
@@ -40,35 +39,31 @@ wire [23:0] flashmem_addr = (FLASH_BEGIN_ADDR + (index_lat << 18)) | {load_addr}
 reg [3:0] index_lat;
 reg load_done_pre;
 
-reg [7:0] wait_ctr = 0;
-
 // Flash memory load interface
 always @(posedge clock) 
 begin
   if (reset == 1'b1) begin
     load_done_pre <= 1'b0;
     load_done <= 1'b0;
-    load_addr <= 20'h0000;
-    wait_ctr <= 8'h00;
+    load_addr <= 0;
     index_lat <= 4'h0;
   end else begin
     if (reload == 1'b1) begin
       load_done_pre <= 1'b0;
       load_done <= 1'b0;
-      load_addr <= 20'h0000;
-      wait_ctr <= 8'h00;
+      load_addr <= 0;
       index_lat <= index;
     end else begin
       if(!load_done_pre) begin
         if (flashmem_ready == 1'b1) begin
-          if (load_addr > END_ADDR) begin
+          if (load_addr[$bits(load_addr)-1]) begin
             load_done_pre <= 1'b1;
           end else 
-            load_addr <= load_addr + 1'b1;
+            load_addr <= load_addr + 1;
         end
       end else begin
-        if (wait_ctr < 8'hFF)
-          wait_ctr <= wait_ctr + 1;
+        if (load_addr[9] == 0)
+          load_addr <= load_addr + 1;
         else
           load_done <= 1'b1;
       end
@@ -76,7 +71,9 @@ begin
   end
 end
 
-icosoc_flashmem flash_i (
+flashmem
+flashmem_i
+(
   .clk(clock),
   .reset(reset),
   .valid(flashmem_valid),
